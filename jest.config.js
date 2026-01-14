01a650ac9e004b5d33b8c85f40d7c12e38cf6f81
AlGold777 <125289091+AlGold777@users.noreply.github.com>
2026-01-14 23:21:49 +0100

    Revert "verson_2.70.46"
    This reverts commit 3b8a98a756472fbe51d875b18bc4fff634bf1693.

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: 'jsdom',
-  roots: ['<rootDir>/tests'],
-  moduleFileExtensions: ['js', 'json'],
-  setupFilesAfterEnv: ['<rootDir>/tests/setupEnv.js']
-};
